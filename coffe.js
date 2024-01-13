9ade414feadd2e4d35de9bd9dd03d36fc84197b7
adam12567798980990897654 <156542021+adam12567798980990897654@users.noreply.github.com>
2024-01-13 20:14:24 +0100

    Add files via upload

---
+++ coffe.js
@@ -0,0 +1,12 @@
+function g1(x)
+{
+document.getElementById(x).style.fontSize='30px';
+}
+function g2(x)
+{
+    document.getElementById(x).style.fontSize='29px'; 
+}
+function i(x)
+{
+    document.images[0].style.width='30px';
+}
